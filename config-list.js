74de559e4c37b623890b496b442d8f39e63b2f0d
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-11-24 22:02:13 +0800

    西元2023年11月24日 (週五) 22時02分13秒 CST

--- config-list.js
+++ config-list.js
@@ -527,20 +527,20 @@ let feedList = [
 ]
 // ----------------------------------------------------------------
 
-feedList = [
-  { // <outline type="rss" text="學不完．教不停．用不盡 :: 痞客邦 PIXNET ::" title="學不完．教不停．用不盡 :: 痞客邦 PIXNET ::" xmlUrl="http://feeds.feedburner.com/pixnetisvincent"/>
-    title: '公視新聞網',
-    feedID: 'pts-news',
-    feedURL: 'https://news.pts.org.tw/xml/newsfeed.xml',
-    itemFilters: [
-      ItemFilters['sites'].ItemFilterBlog,
-      ItemFilters['sites'].ItemFilterPTSNews,
-    ],
-    options: {
-      selector: 'body > article',
-      proxy: true,
-    }
-  },
-]
+// feedList = [
+//   { // <outline type="rss" text="學不完．教不停．用不盡 :: 痞客邦 PIXNET ::" title="學不完．教不停．用不盡 :: 痞客邦 PIXNET ::" xmlUrl="http://feeds.feedburner.com/pixnetisvincent"/>
+//     title: '公視新聞網',
+//     feedID: 'pts-news',
+//     feedURL: 'https://news.pts.org.tw/xml/newsfeed.xml',
+//     itemFilters: [
+//       ItemFilters['sites'].ItemFilterBlog,
+//       ItemFilters['sites'].ItemFilterPTSNews,
+//     ],
+//     options: {
+//       selector: 'body > article',
+//       proxy: true,
+//     }
+//   },
+// ]
 
 module.exports = feedList
